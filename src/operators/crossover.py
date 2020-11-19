6bbb134dd5c36a8bed3dc983572430b1d9df4b9a
Krzysztof <kzajac1997@gmail.com>
2020-11-19 18:42:37 +0100

    Refactor operators

--- src/operators/crossover.py
+++ src/operators/crossover.py
@@ -21,20 +21,6 @@ def hypersphere_crossover(individuals: np.array) -> np.array:
     return (translation_length * offset_vector) + x
 
 
-def gaussian_mutation(individual: np.array, mutation_strength: float = 0.1) -> np.array:
-    """
-    Performs Gaussian mutation operation on single individual in floating point gene representation,
-    Mutation works by drawing N samples from random normal distribution add adding in to individual
-
-    :param individual: single solution represented as numpy array with shape (N_OBJECTIVE_DIMS, )
-    :param mutation_strength: strength of mutation operation, should be adjusted based on search space size
-
-    :return: Mutated individual solution
-    """
-    offset = np.random.randn(individual.shape[0])
-    return mutation_strength * offset + individual
-
-
 def vectorized_hypersphere_crossover(mating_pool: np.array, n_offspring: int) -> np.array:
     """
     Crossover between two individuals applied to whole population in floating point gene representation
@@ -56,17 +42,3 @@ def vectorized_hypersphere_crossover(mating_pool: np.array, n_offspring: int) ->
     offsets = y - x
     lengths = np.random.normal(0.5, 0.25, n_offspring)
     return row_wise_multiply(offsets, lengths) + x
-
-
-def vectorized_gaussian_mutation(population: np.array, mutation_strength: float = 0.1) -> np.array:
-    """
-    Performs Gaussian mutation operation on population of individuals in floating point gene representation,
-    Mutation works by drawing N samples from random normal distribution add adding in to individual
-
-    :param population: array of solutions, with shape (N_INDIVIDUALS, N_OBJECTIVE_DIMS, )
-    :param mutation_strength: strength of mutation operation, should be adjusted based on search space size
-
-    :return: array containing mutated population
-    """
-    offset = np.random.randn(population.shape[0] * population.shape[1]).reshape(population.shape)
-    return mutation_strength * offset + population
